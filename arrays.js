ac9ac8b659c1dfc8add9cb996a8d0840c0e1840a
Lauren Seward <lauren.unal@gmail.com>
2022-10-18 19:43:45 -0500

    Add array methods + comments

--- arrays.js
+++ arrays.js
@@ -2,4 +2,13 @@ var catNames = ['fernie', 'frodo', 'phillip', 'frankie'];
 var ages = [1, 2, 3, 4];
 var areTheyHappy = [true, false, true, true]
 
+// Utilizing pop() method will print out ['fernie', 'frodo', 'phillip']
+catNames.pop();
+console.log(catNames);
+// Utilizing push() will add additonal element 5 to end of array
+ages.push(5);
+console.log(ages);
+// Utilizing unshift method will add an element to beginning of array
+areTheyHappy.unshift(true);
+console.log(areTheyHappy);
 
